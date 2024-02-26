923101f1eca64cddd3e3fa6b90c3e753b122deed
gustavosuisso <0123guga@gmail.com>
2024-02-25 23:40:34 -0300

    Criando a classe Carro

---
+++ Model/Carro.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesafioEstacionamento.Model
+{
+
+    // classe onde armazenaremos apenas o número da placa do carro
+    public class Carro
+    {
+        // campo onde armazenaremos a placa informada
+        private string _placa;
+
+        // atributo que usaremos para passar o valor e pegar o valor do campo (ou atributo) _placa
+        public string Placa 
+        {
+
+            get => _placa.ToUpper(); // usando body expressions para abreviar o get
+
+            set
+            {
+                if(value != "")
+                {
+                    _placa = value;
+                } 
+                else 
+                {
+                    throw new ArgumentException();
+                }
+            }
+
+        }
+    }
+}
